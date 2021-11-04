794b77a259626815d55f00e7970f4606d93f40c5
Abdulroqeeb <daramolaabdulroqeeb@gmail.com>
2021-11-04 11:59:28 +0100

    :construction: Project Folder Set up

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './Pages/LandingPage/landingPage';
-
 import './App.css';
 
 
 function App() {
   return (
-    <>
-      <LandingPage/>
-    </>
+    <Router>
+      <div>
+      <Routes>
+        <Route path='/' element={<LandingPage />}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+
+
