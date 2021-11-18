7588f9dc93d294aa6168a54e126128a7e52dbb2a
Abdulroqeeb <daramolaabdulroqeeb@gmail.com>
2021-11-18 11:18:24 +0100

    fixing

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -2,13 +2,7 @@ import React from 'react'
 import Logo from '../../Assets/Logo1-min.png'
 import './header.css';
 import { Link } from 'react-router-dom';
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
 import Login from './../login/login';
-=======
->>>>>>> master
->>>>>>> staging
 
 const header = () => {
     return (
@@ -18,12 +12,8 @@ const header = () => {
                     <img className={headerOne.logo} src={Logo} alt="Aphrodite logo"/>
                 </div>
                 <div>
-<<<<<<< HEAD
-                    <button className={headerOne.btnSignIn}>Sign In</button>
-=======
-                   <Link to={<Login />}> <button className={headerOne.btnSignIn}>Sign In</button> </Link> 
->>>>>>> staging
-                </div>
+                  <Link to={<Login />}> <button className={headerOne.btnSignIn}>Sign In</button> </Link> 
+             </div>
             </div> 
             <div className={headerOne.heroContent}>
                 <h1 className={headerOne.heroH1}>
@@ -35,13 +25,9 @@ const header = () => {
                     Don't have an account yet? Sign Up now and <br/>
                     get up to meet your new partner.
                 </p>
-<<<<<<< HEAD
-                <Link to ='/verify'><button className={headerOne.heroGetStarted}>GET STARTED</button></Link>
-=======
                 <Link to="/signup">
                 <button className={headerOne.heroGetStarted}>GET STARTED</button>
                 </Link> 
->>>>>>> staging
             </div>
         </div>
     )
